d45b329c957f4f3537c37688dee7c43ad315e40c
akdoganalibugra1 <akdoganalibugra@gmail.com>
2024-02-08 15:15:19 +0300

    creating component

---
+++ react-app/src/Message.tsx
@@ -0,0 +1,12 @@
+
+// PascalCasing
+function Message() {
+    // JSX Javascript XML
+    const name = 'Ali';
+    if(name)
+        return <h1>Hello {name}</h1>
+
+    return <h1>Hello World</h1>
+}
+
+export default Message;
